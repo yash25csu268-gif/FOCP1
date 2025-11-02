691399750004e84a5411590406c878e4c7cc3f79
YASH BHATI <yash25csu268@ncuindia.edu>
2025-11-02 15:14:31 +0530

    Q8.C

---
+++ Q8.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int t1 = 0, t2 = 1;
+    int nextTerm;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= n; ++i) {
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+
+    return 0;
+}
